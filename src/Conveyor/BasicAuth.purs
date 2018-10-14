fc16c7d2d45995ca5c427e61fb2f3c1fd213b634
Shinya Takahashi <s.takahashi313@gmail.com>
2018-10-14 23:58:18 +0900

    Respond header

--- src/Conveyor/BasicAuth.purs
+++ src/Conveyor/BasicAuth.purs
@@ -6,23 +6,32 @@ import Conveyor.Servable (class Servable, serve)
 import Conveyor.Types (Responder(..))
 import Data.Maybe (Maybe(..))
 import Effect.Class (liftEffect)
-import Foreign (unsafeToForeign)
 import Node.BasicAuth (Credentials, authenticate)
+import Node.HTTP (setHeader)
+import Simple.JSON (write)
 
 
 
-data BasicAuth server = BasicAuth (Maybe Credentials) server
+type Settings =
+  { realm :: String
+  , credentials :: Credentials
+  }
+
+data BasicAuth server = BasicAuth (Maybe Settings) server
 
 instance servableBasicAuth :: Servable ex server => Servable ex (BasicAuth server) where
-  serve (BasicAuth mCred server) extraData rawData =
-    case mCred of
+  serve (BasicAuth mSetting server) extraData rawData =
+    case mSetting of
       Nothing -> serve server extraData rawData
-      Just cred -> do
-        ok <- liftEffect $ authenticate cred rawData.req
+      Just s -> do
+        ok <- liftEffect $ authenticate s.credentials rawData.req
         if ok
           then serve server extraData rawData
-          else pure $ Responder
-            { contentType: "text/plain; charset=utf-8"
-            , code: 401
-            , body: unsafeToForeign ""
-            }
+          else do
+            liftEffect $ setHeader rawData.res "WWW-Authenticate" $ "Basic realm=\"" <> s.realm <> "\""
+            pure $ Responder
+              { contentType: "application/json; charset=utf-8"
+              , code: 401
+              , body: write { message: "Unauthorized" }
+              }
+
