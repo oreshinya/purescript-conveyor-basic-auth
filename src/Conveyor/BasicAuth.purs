73c77901bd7d7717eee77b2066ab9bab7120302f
Shinya Takahashi <s.takahashi313@gmail.com>
2018-10-14 14:58:43 +0900

    Make skippable

--- src/Conveyor/BasicAuth.purs
+++ src/Conveyor/BasicAuth.purs
@@ -4,23 +4,25 @@ import Prelude
 
 import Conveyor.Servable (class Servable, serve)
 import Conveyor.Types (Responder(..))
+import Data.Maybe (Maybe(..))
 import Effect.Class (liftEffect)
 import Foreign (unsafeToForeign)
 import Node.BasicAuth (Credentials, authenticate)
 
 
 
-data BasicAuth server = BasicAuth Credentials server
+data BasicAuth server = BasicAuth (Maybe Credentials) server
 
 instance servableBasicAuth :: Servable ex server => Servable ex (BasicAuth server) where
-  serve (BasicAuth cred server) extraData rawData = do
-    ok <- liftEffect $ authenticate cred rawData.req
-    if ok
-      then
-        serve server extraData rawData
-      else
-        pure $ Responder
-          { contentType: "text/plain; charset=utf-8"
-          , code: 401
-          , body: unsafeToForeign ""
-          }
+  serve (BasicAuth mCred server) extraData rawData =
+    case mCred of
+      Nothing -> serve server extraData rawData
+      Just cred -> do
+        ok <- liftEffect $ authenticate cred rawData.req
+        if ok
+          then serve server extraData rawData
+          else pure $ Responder
+            { contentType: "text/plain; charset=utf-8"
+            , code: 401
+            , body: unsafeToForeign ""
+            }
